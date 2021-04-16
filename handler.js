009f26167d648a5d1ac83c94f52f5b200cfbbf23
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-04-17 04:48:39 +0700

    Add files via upload

--- handler.js
+++ handler.js
@@ -478,7 +478,7 @@ const toBase64 = (gambar) => new Promise(async (resolve, reject) => {
 			if (!msg.message) return
                  msg.message = (Object.keys(msg.message)[0] === 'ephemeralMessage') ? msg.message.ephemeralMessage.message : msg.message
 			if (msg.key && msg.key.remoteJid == 'status@broadcast') return 
-       if (!msg.key.fromMe) return
+      // if (!msg.key.fromMe) return
 			m = simple.smsg(caliph, msg)
 			if (m.isBaileys) return
 			//if (!msg.key.fromMe) return 
@@ -3147,7 +3147,7 @@ addFilter(sender)
 					
 addFilter(sender)
 					break
-				case prefix+'demote':
+				case prefix+'demotesssisj':
 					 
 					if (!isUser) return reply(mess.only.userB)
 					if (isBanned) return reply(mess.only.benned)   
@@ -3162,7 +3162,7 @@ addFilter(sender)
                      }
 				    addFilter(sender)
 					break
-                  case prefix+'promote':
+                  case prefix+'sksnkdns':
 					 
 					if (!isUser) return reply(mess.only.userB)
 					if (isBanned) return reply(mess.only.benned)   
