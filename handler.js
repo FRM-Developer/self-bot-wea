a7db71d2ef75d4050bd5e749d4d072340a3e1a58
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-05-10 10:12:35 +0000

    Update handler.js

--- handler.js
+++ handler.js
@@ -1874,35 +1874,44 @@ addFilter(sender)
 addFilter(sender)
 					break
               
-				case prefix+'tiktok':
-
-					  if (isLimit(sender)) return reply(`Maaf ${pushname}, Kuota Limit Kamu Sudah Habis, Ketik ${prefix}limit Untuk Mengecek Kuota Limit Kamu`)
-
-					if (!isUser) return reply(mess.only.userB)
-
-					if (isBanned) return reply(mess.only.benned)   
-
-					if (args.length < 1) return reply('Urlnya mana um?')
-
-					if (!isUrl(args[0]) && !args[0].includes('tiktok.com')) return reply(mess.error.Iv)
-
-					reply(mess.wait)
-
-async function getVideo(URL) {
-    sstiktod = await fetchJson(`http://192.53.115.44/sstik?url=${URL}`)
-    h = await sstiktod.mp4direct
-    return h
-}
-
-buffer = await getBuffer(await getVideo(args[0]))
-
-					caliph.sendMessage(from, buffer, video, {quoted:msg, mimetype:'video/mp4'})
-
-					limitAdd(sender)
-
-addFilter(sender)
-
-					break
+			case prefix+'tiktok':
+									  if (isLimit(sender)) return reply(`Maaf ${pushname}, Kuota Limit Kamu Sudah Habis, Ketik ${prefix}limit Untuk Mengecek Kuota Limit Kamu`)
+									if (!isUser) return reply(mess.only.userB)
+									if (isBanned) return reply(mess.only.benned)   
+									if (args.length < 1) return reply('Urlnya mana um?')
+									if (!isUrl(args[0]) && !args[0].includes('tiktok.com')) return reply(mess.error.Iv)
+									reply(mess.wait)
+									const puppeteer = require("puppeteer");
+				
+				async function getVideo(URL) {
+					    const browser = await puppeteer.launch({
+						            headless: true,
+						            args: ['--no-sandbox', '--disable-setuid-sandbox']
+					    });
+					    const page = await browser.newPage();
+					    await page.goto('https://snaptik.app/');
+					
+					    await page.type('#url', `${URL}`);
+					    await page.click('#send', { delay: 300 });
+					
+					    await page.waitForSelector('#download-block > div > a:nth-child(3)', {delay: 300});
+					    let mp4direct = await page.$eval("#download-block > div > a:nth-child(3)", (element) => {
+						            return element.getAttribute("href");
+					    });
+					    let image = await page.$eval("#div_download > section > div > div > div > article > div.zhay-left.left > img", (element) => {
+						            return element.getAttribute("src");
+					    });
+						let textInfo = await page.$eval('#div_download > section > div > div > div > article > div.zhay-middle.center > p:nth-child(2) > span', el => el.innerText);
+						let nameInfo = await page.$eval('#div_download > section > div > div > div > article > div.zhay-middle.center > h1 > a', el => el.innerText);
+						let timeInfo = await page.$eval('#div_download > section > div > div > div > article > div.zhay-middle.center > p:nth-child(3) > b', el => el.innerText);
+						browser.close();
+					    return { mp4direct, image, textInfo, nameInfo, timeInfo }
+				}
+				buffer = await getVideo(isUrl(args.join(' ')))
+									caliph.sendMessage(from, await getBuffer(buffer.mp4direct), video, {quoted:msg, mimetype:'video/mp4'})
+									limitAdd(sender)
+				addFilter(sender)
+									break
                case prefix+'tiktokwm':
                  if (isLimit(sender)) return reply(`Maaf ${pushname}, Kuota Limit Kamu Sudah Habis, Ketik ${prefix}limit Untuk Mengecek Kuota Limit Kamu`)
 					if (!isUser) return reply(mess.only.userB)
